e95682316408b6d60a3f78a243027ab982635a27
AnastasiyaNovikovaa <43859713+AnastasiyaNovikovaa@users.noreply.github.com>
2018-12-13 19:09:49 +0300

    Update Tree.h

--- Laba3_3(alg)/Tree.h
+++ Laba3_3(alg)/Tree.h
@@ -1,22 +1,18 @@
 #pragma once
 #include "Iterator.h"
-#include <stack>
-#include <queue>
 #include <iomanip>
 #include <iostream>
 #include "../../3.2 алг/3.2 алг/Class.h"
 #include "../../3.2 алг/3.2 алг/Class.cpp"
 
 class Tree
-{
-public:
-
+{  
+   private:
 	class Node//элемент дерева
 	{
 	public:
 		
 		Node() {};
-
 		int data;//значение
 		Node *next = next;
 		Node *rgt = nullptr;//правый узел
@@ -24,30 +20,34 @@ public:
 		Node *parent = nullptr;//батюшка родимый
 		Node(int k);
 		void Delete();
-	private:
+		 
+	     private:
 		~Node();
-	};
-	
+	 };
+  public:
 	Tree() {  };
 	Node *root = nullptr;//корень дерева
 	Node *last = nullptr;//последний элемент дерева
 	Node *prevlast = nullptr;
 	size_t size = 0;
-
-
-	void insert(int newelem);
-	void print_Tree(Node * p, int level);
 	void TreeSort(Node* elem);
+	void insert(int newelem);
+	void showTree(Node* root, int p, int s);
 	Iterator * Dcreate_iterator()const;
 	Iterator * Bcreate_iterator()const;
 	void BackTravers(Node* root, void *null);
 	void SymTravers(Node* root, void *null);
 	void DirectTravers(Node* root, void *null);
-	int Search(Node *root, int elem);
-	int remove(int elem, Node *node);
-	private:
+	bool Search(int findelem);
+	void remove(int elem);
 	bool isEmpty(Node *root);
+	~Tree();
+
+private:
 	Tree(int key);
+	void showLine(const char* c, int p, int s);
+	void help_Search(int findelem, Node *node);
+	bool help_Search_in_search(Node *node, int findelem);
 	
 	class DIterator : public Iterator
 	{
@@ -79,4 +79,3 @@ public:
 		Node * current;
 	};
 };
-
